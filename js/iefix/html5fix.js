17a9d4059f5f4b70e37c59461b3a2ac9053ced32
Zilant <93409915+the-zilant@users.noreply.github.com>
2022-01-17 15:12:36 +0500

    Delete js directory

--- js/iefix/html5fix.js
+++
@@ -1,8 +0,0 @@
-document.createElement('header');
-document.createElement('nav');
-document.createElement('section');
-document.createElement('article');
-document.createElement('aside');
-document.createElement('footer');
-document.createElement('figure');
-document.createElement('figcaption');
